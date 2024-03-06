2f0ce07ee1f2883b5834dd75137ff23320cce357
nasser30 <mohamedsalheen1@gmail.com>
2024-03-07 01:38:34 +0200

    Frontend Login & Register & Base Pages

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path 
+from account.views import *
 
 urlpatterns = [
-    
+    path('base/', home, name='home'),
+
+    # path('news-list/', news_list, name='news_listing'),
+    # path('news-detail/<int:news_id>/', news_detail, name='news_detail'),
+    path('donate/', donate, name='donate'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponse
+# from django.http import HttpResponseRedirect
+# from .models import *
+# from .forms import *
 
 # Create your views here.
+def home(request):
+  return render (request, 'base.html')
+
+# def news_list(request):
+#     return render(request, 'account/news.html')
+
+# def news_detail(request):
+#     return render(request, 'account/news-detail.html')
+
+def donate(request):
+    return render(request, 'account/donate.html')
+
+def login(request):
+    return render(request, 'account/login.html')
+
+def register(request):
+    return render(request, 'account/register.html')
