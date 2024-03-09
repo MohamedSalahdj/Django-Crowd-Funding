3d30ade94046e9de799aba45a9317c855e53784b
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-03-09 18:41:26 +0200

    fix conflicts with main

--- project/settings.py
+++ project/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'campaign.apps.CampaignConfig',
     'reports.apps.ReportsConfig',
   
-    
 ]
 
 
@@ -158,10 +157,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_HOST_USER = 'teamdjango19@gmail.com'
 # EMAIL_HOST_PASSWORD = 'Djangoteam123'
 # EMAIL_USE_TLS = True
+#
