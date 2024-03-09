0412f08dcfc75996b8ef1586ce81fffb410b92f1
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-03-09 15:41:46 +0200

    EmailBackend

--- project/settings.py
+++ project/settings.py
@@ -159,6 +159,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'teamdjango19@gmail.com'
-EMAIL_HOST_PASSWORD = 'Djangoteam123'
+EMAIL_HOST_PASSWORD = 'kdcl rwhs rhhd jjhm'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'shahd3a7med@gmail.com'
