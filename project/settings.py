c6aeda3f40f537110aee1d422e6f7d17ec8a2972
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-03-09 19:05:55 +0200

    fix conflicts

--- project/settings.py
+++ project/settings.py
@@ -155,12 +155,12 @@ AUTHENTICATION_BACKENDS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'teamdjango19@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Djangoteam123'
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'teamdjango19@gmail.com'
+EMAIL_HOST_PASSWORD = 'hpms wlia ikjs qfzw'
+EMAIL_USE_TLS = True
